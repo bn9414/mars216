a82939312c283957cbc00a8269f5558617cffc94
Bhuvanesh <bhuvaneshnick@gmail.com>
2018-05-17 18:16:46 +0530

    sd

---
+++ uc/about-us.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- s: font family-->
+ <link href="https://fonts.googleapis.com/css?family=Lato|Roboto" rel="stylesheet">
+    <!-- e: font famil-->
+    
+        <title>About us | Know who we are</title>
+    <meta name="description" content="content need to be updated">
+    
+
+    
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="common/common.css" type="text/css" rel="stylesheet">  
+      <link href="about-us/about.css" type="text/css" rel="stylesheet"> 
+              
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+    
+    <script type="text/javascript">
+        
+var jQuery_1_5_2= $.noConflict(true);
+        
+</script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>  
+    
+   
+    
+</head>
+<body class="about-us">
+    
+ 
+  <div class="se-pre-con"></div>
+<!--topnavi-->
+    <div class="top-nav">
+        <div class="lft-prt">
+            <a href="#"><img src="common/images/phone.svg">+91 9020230230</a>
+            <a href="#"><img src="common/images/envp.svg"> demo@gmail.com</a>
+        </div>
+        <div class="rgt-prt">
+<!--            <div class="rt-btncon">
+            <button class="btn-pln">LETS PLAN</button>
+            </div>-->
+            <a href="#"><img src="common/images/fac.svg"></a>
+            <a href="#"><img src="common/images/twt.svg"></a>
+            <a href="#"><img src="common/images/in.svg"></a>
+            <a href="#"><img src="common/images/gog.svg"></a>
+            <a href="#"><img src="common/images/insta.svg"></a>
+        </div>
+    </div>
+    <!--topnavi-->
+    <!--navigation-->
+    <div class="nav" id="nav">
+        <div class="brand">
+         <a href="index.html"><img src="common/images/logo1.svg" alt=""></a> 
+       </div>
+       <div class="nav-web">
+       <a href="wedding_index.html" class="web-mnu ">Wedding Event</a>
+        <a href="socialevent.html" class="web-mnu">Social Event</a>
+        <a href="live.html" class="web-mnu">Live Event</a>
+        <a href="corporate.html" class="web-mnu">Corporate Event</a>
+        <a href="brand.html" class="web-mnu">Brand Management</a>
+           <div class="bar-con1" id="bar-con1">
+            <div class="bar1 bar1" id="bar-s1"></div>
+            <div class="bar1 bar2" id="bar-s2"></div>
+            <div class="bar1 bar3" id="bar-s3"></div>
+            </div>
+       </div>
+        <div class="sub-mne" id="submnu1">
+           <a href="about-us.html">About us</a>
+        <a href="contact.html">Contact us</a>
+        <a href="career.html">Career</a>
+           </div>
+       <div class="navlst" id="navlst">
+        <a href="wedding_index.html">Wedding Event</a>
+        <a href="socialevent.html" >Social Event</a>
+        <a href="live.html">Live Event</a>
+        <a href="corporate.html">Corporate Event</a>
+        <a href="brand.html">Brand Management</a>
+        <div class="sub-mne">
+           <a href="about-us.html">About us</a>
+        <a href="contact.html">Contact us</a>
+        <a href="career.html">Career</a>
+           </div>
+       </div>
+       <div class="bar-con" id="bar-con">
+            <div class="bar bar1" id="bar1"></div>
+            <div class="bar bar2" id="bar2"></div>
+            <div class="bar bar3" id="bar3"></div>
+       </div>
+    </div>
+    <!--navigation end-->
+
+<!-- s: content area-->
+ 
+    
+    <div class="keyword">
+    <h1>When it comes to organising events we are,</h1>
+    <div class="keybox">
+        <div class="keyb1"><div class="kimg "></div><p class="ktxt" >Creative</p></div>
+        
+        <div class="keyb1"><div class="kimg kb2"></div><p class="ktxt" >Passionate</p></div>
+        
+        <div class="keyb1"><div class="kimg kb3"></div><p class="ktxt" >Cost effective</p></div>
+        
+        <div class="keyb1"><div class="kimg kb4"></div><p class="ktxt" >Fun</p></div>
+        
+        </div>
+        
+    </div>
+    
+    <div class="whwh">
+        <div class="whwh_hd">
+            <div class="whimg"></div>
+            <div class="whtxt"><p>ho we are <span>&amp;</span></p><p>hat we do.</p></div>
+        
+        </div>
+    <div class="whdes">
+        
+        <p>Here goes a text and tells who you are and what you do clearly.Here goes a text and tells who you are and what you do clearly.Here goes a text and tells who you are and what you do clearly.Here goes a text and tells who you are and what you do clearly.</p><br/>
+        
+           <p>Here goes a text and tells who you are and what you do clearly.Here goes a text and tells who you are and what you do clearly.Here goes a text and tells who you are and what you do clearly.Here goes a text and tells who you are and what you do clearly.</p>
+        
+        </div>
+    
+    </div>
+
+    
+    
+    <div class="mv">
+        <div>
+        <h1 class="mist">Missi<span class="jtar"></span>n</h1>
+    <p>Our mission is to provide unforgettable experience and value for money with our innovation &amp; creativity to every client </p>
+    
+            </div>
+            <div>         <h1 class="vist" >visi<span class="jtar"></span>n</h1>
+    <p>To become a resource hub nationally &amp; globally in Entertainment, Media &amp; Event industry.  </p>
+    
+        </div>
+
+    </div>
+    <!-- e: content area-->
+             
+
+    <!-- s: Footer-->
+    
+
+       <div class="footer-sec">
+                <div class="fir-div">
+                    <div class="col-3">
+                         <a href="#" class="lgo"><img src="common/images/logo1.svg" alt=""></a> 
+                        <p class="foot-p">Marsala is 'ONE STOP SHOP' for all event-needs.At Marsala we strive to be the most reliable and creative event company ,always delivering a high standard of quality and value for money for our customers.We work in close partnership width our customers.</p>
+                        
+                    </div>
+                    <div class="col-3">
+                        <h2 class="fot-h2">Keep in <span class="sep-clr">Touch</span></h2>
+                        <ul class="list-img">
+                            <li>PHOENIX MARKT CITY
+                                PHOENIX MARKT CITY,NO,142,
+                                OLD DOOR NO.66,VELACHERY RD,
+                                INDIRA GANDHI NAGAR,VELACHERY,
+                                CHENNAI,TAMIL NADU 600042,INDIA
+                               </li>
+                        </ul>
+                    </div>
+                    <div class="col-3">
+                        <h2 class="fnt-fot">Event <span class="sep-clr">Gallery</span></h2>
+                        <div class="gal-div">
+                        <a href=""><img src="common/images/gal1.jpg" class="sl-img"></a>
+                        <a href=""><img src="common/images/gal2.jpeg" class="sl-img"></a>
+                        <a href=""><img src="common/images/gal3.jpeg" class="sl-img"></a>
+                        <a href=""><img src="common/images/gal4.jpeg" class="sl-img"></a>
+                        </div>
+                         <div class="gal-div">
+                        <a href=""><img src="common/images/gal5.jpeg" class="sl-img"></a>
+                        <a href=""><img src="common/images/gal6.jpg" class="sl-img"></a>
+                        <a href=""><img src="common/images/gal7.jpeg" class="sl-img"></a>
+                        <a href=""><img src="common/images/gal8.jpg" class="sl-img"></a>
+                        
+                        </div>
+                    </div>
+                    <div class="col-3">
+                         <h2 class="fnt-fot">Quick <span class="sep-clr">Links</span></h2>
+                        <ul class="list-sty">
+                        <li><a href="">OUR SERVICES</a></li>
+                            <li><a href="">ABOUT US</a></li>
+                            <li><a href="">BLOGS</a></li>
+                            <li><a href="">CAREER</a></li>
+                        </ul>
+                        <div class="">
+                        <a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        <a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        <a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        <a href=""><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    <div class="cpy">
+        <div class="cpy-txt">
+            <p>Marsala 2018.All rights reserved</p>
+        </div>
+    </div>
+        <!-- e: Footer-->
+       <script src="common/common.js" >
+    </script>
+     
+      <script src="about-us/about.js" >
+    </script>
+
+             
+          
+    
+    
+    
+    
+    </body>
